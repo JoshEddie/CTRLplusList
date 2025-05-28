2f65ca9cf6bad90591b6f9672c7cf500b83ee34b
Josh <90493234+JoshEddie@users.noreply.github.com>
2025-05-28 07:28:34 -0500

    beta-0.5: DND Reorder list

--- app/(main)/items/ui/components/Items.tsx
+++ app/(main)/items/ui/components/Items.tsx
@@ -1,29 +1,5 @@
-'use client';
-
 import Empty from '@/app/ui/components/Empty';
-import { ItemDetails, ItemStoreTable, ItemTable } from '@/lib/types';
-import {
-  closestCenter,
-  DndContext,
-  DragEndEvent,
-  DragOverlay,
-  DragStartEvent,
-  KeyboardSensor,
-  MouseSensor,
-  TouchSensor,
-  UniqueIdentifier,
-  useSensor,
-  useSensors
-} from '@dnd-kit/core';
-import {
-  arrayMove,
-  rectSortingStrategy,
-  SortableContext,
-  sortableKeyboardCoordinates,
-  useSortable,
-} from '@dnd-kit/sortable';
-import { useState } from 'react';
-import { RxDragHandleDots2 } from 'react-icons/rx';
+import { ItemStoreTable, ItemTable } from '@/lib/types';
 import Item from './Item';
 
 interface ItemsProps {
@@ -31,156 +7,22 @@ interface ItemsProps {
   user_id?: string;
 }
 
-export default function Items({ items, user_id }: ItemsProps) {
-  const [itemsState, setItemsState] = useState(items);
-  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
-  const [isDragging, setIsDragging] = useState(false);
-
-  const sensors = useSensors(
-    useSensor(MouseSensor, {
-      activationConstraint: {
-        distance: 10,
-      },
-    }),
-    useSensor(TouchSensor, {
-      activationConstraint: {
-        delay: 250,
-        tolerance: 5,
-      },
-    }),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
-
-  const getIndex = (id: UniqueIdentifier) =>
-    itemsState.findIndex((item) => item.id === id);
-  const activeIndex = activeId != null ? getIndex(activeId) : -1;
-
-  const handleDragStart = (event: DragStartEvent) => {
-    setIsDragging(true);
-    setActiveId(event.active.id);
-  };
-
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
-    setIsDragging(false);
-
-    if (!over) {
-      setActiveId(null);
-      return;
-    }
-
-    if (active.id !== over.id) {
-      const oldIndex = getIndex(active.id);
-      const newIndex = getIndex(over.id);
-      
-      setItemsState((items) => arrayMove(items, oldIndex, newIndex));
-    }
-
-    setActiveId(null);
-  };
-
-  const handleDragCancel = () => {
-    setIsDragging(false);
-    setActiveId(null);
-  };
-
-  return itemsState.length === 0 ? (
-    <Empty type="item" />
-  ) : (
-    <DndContext
-      sensors={sensors}
-      collisionDetection={closestCenter}
-      onDragStart={handleDragStart}
-      onDragEnd={handleDragEnd}
-      onDragCancel={handleDragCancel}
-    >
-      <SortableContext
-        items={itemsState.map((item) => item.id)}
-        strategy={rectSortingStrategy}
-      >
-        <div className="item-grid">
-          {itemsState.map((item) => {
-            return (
-              <SortableItem
-                key={item.id}
-                id={item.id}
-                item={item}
-                showEditButton={item.user_id === user_id}
-                isAnyDragging={isDragging}
-              />
-            );
-          })}
-        </div>
-      </SortableContext>
-
-        <DragOverlay>
-          {activeId ? (
-            <Item
-              item={itemsState[activeIndex]}
-              className="item-drag-overlay"
-              showEditButton={false}
-            />
-          ) : null}
-        </DragOverlay>
-    </DndContext>
-  );
-}
-
-export function SortableItem({
-  id,
-  item,
-  className,
-  showEditButton = false,
-  isAnyDragging = false,
-}: {
-  id: string;
-  item: (ItemTable & { stores: ItemStoreTable[] }) | ItemDetails | undefined;
-  className?: string;
-  showEditButton?: boolean;
-  isAnyDragging?: boolean;
-}) {
-  const { 
-    attributes, 
-    listeners, 
-    setNodeRef, 
-    transform, 
-    transition,
-    isDragging 
-  } = useSortable({ id });
-
-  const style = {
-    transform: transform 
-      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
-      : undefined,
-    transition,
-  };
+export default function Items({
+  items,
+  user_id,
+}: ItemsProps) {
 
   return (
-    <div 
-      ref={setNodeRef} 
-      style={style} 
-      data-dragging={isDragging ? "true" : undefined}
-      {...attributes}
-      className={`
-        sortable-item 
-        ${isDragging ? 'is-dragging' : ''} 
-        ${isAnyDragging ? 'drag-active' : ''}
-        ${className || ''}
-      `.trim().replace(/\s+/g, ' ')}
-    >
-      <div 
-        className="drag-handle" 
-        {...listeners}
-        aria-label="Drag to reorder"
-      >
-        <RxDragHandleDots2 size={24}/>
-      </div>
-      <Item
-        item={item}
-        showEditButton={showEditButton}
-      />
+    items.length === 0 ? (
+      <Empty type="item" />
+    ) : (
+    <div className="item-grid">
+      {items.map((item) => {
+        return (
+          <Item key={item.id} item={item} showEditButton={item.user_id === user_id} />
+        )
+      })}
     </div>
+    )
   );
 }
